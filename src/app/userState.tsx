7264f71b967977ef12076ad6b62e58f5bac156c7
Nawamin Onkhwan <119128184+Carely-Dev@users.noreply.github.com>
2025-03-15 20:12:39 +0700

    update

--- src/app/userState.tsx
+++ src/app/userState.tsx
@@ -1,40 +1,29 @@
-// userState.js
-// let currentUser = null;
-
-// export function setUser(user) {
-//   currentUser = user;
-// }
-
-// export function getUser() {
-//   return currentUser;
-// }
 "use client";
-import { useRouter } from 'next/router';
-import { createContext, useContext, useState, useEffect } from 'react';
-
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useState, useEffect } from "react";
 
 interface UserContextProps {
   user: string | null;
   setUserHandle: (user: string) => void;
   logout: () => void;
 }
+
 export const UserContext = createContext<UserContextProps>({
   user: null,
-  setUserHandle: () => { },
-  logout: () => { }
-}
-);
+  setUserHandle: () => {},
+  logout: () => {},
+});
 
 export const useUser = () => {
   return useContext(UserContext);
-}
+};
 
 export const UserContextProvider = ({ children }: { children: React.ReactNode }) => {
-  // const router = useRouter();
   const [user, setUser] = useState<string | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
-    const storedUser = localStorage.getItem('user');
+    const storedUser = localStorage.getItem("user");
     if (storedUser) {
       setUser(storedUser);
     }
@@ -42,27 +31,20 @@ export const UserContextProvider = ({ children }: { children: React.ReactNode })
 
   const setUserHandle = (user: string) => {
     setUser(user);
-    localStorage.setItem('user', user);
+    localStorage.setItem("user", user);
   };
 
   const logout = () => {
-    setUser(null)
-    localStorage.removeItem('user');
+    setUser(null);
+    localStorage.removeItem("user");
+    router.push("/"); // เปลี่ยน route ไปที่หน้า Home หลัง logout
+  };
 
-  }
-
-
-  const value = {
-    user,
-    setUserHandle,
-    logout
-  }
+  const value = { user, setUserHandle, logout };
 
   return (
-    <UserContext.Provider
-      value={value}
-    >
-      {children}
-    </UserContext.Provider>
+      <UserContext.Provider value={value}>
+        {children}
+      </UserContext.Provider>
   );
 };
